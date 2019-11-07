237913d7170047d5604a8cabf3823222be4d0776
Sameer Bhandari <sameer@mcssoftwaresolutions.com>
2019-11-07 12:20:10 -0500

    Add files via upload

--- server.js
+++ server.js
@@ -1,9 +1,44 @@
 const path = require('path');
 const express = require('express');
+const helmet = require('helmet');
+const session = require('cookie-session');
 
 const app = express(),
     DIST_DIR = __dirname,
     HTML_FILE = path.join(DIST_DIR, 'index.html')
+
+
+app.use(helmet());
+//disable X-Powered-By header
+app.disable('x-powered-by');
+
+// Set cookie security options
+var expiryDate = new Date(Date.now() + (2 * 24 * 60 * 60 * 1000)) // 2 days
+app.use(session({
+    name: 'session',
+    keys: ['Q~T~sf6}{*jW)O{6LJ8$SXi,nM1-ws', '3gWsj4p_ovcPC^~Dm|L4cFjBzoSpSZ'],
+    cookie: {
+        secure: true,
+        httpOnly: true,
+        domain: 'liveweb-azapp-prod-eastus2-001.azurewebsites.net',
+        path: '/',
+        expires: expiryDate
+    }
+}));
+
+// set Content security policies
+app.use(helmet.contentSecurityPolicy({
+    directives: {
+        defaultSrc: ["'self'"],
+        styleSrc: ["'self'", 'maxcdn.bootstrapcdn.com', 'fonts.googleapis.com', 'amp.azure.net', "'unsafe-inline'"],
+        scriptSrc: ["'self'", 'amp.azure.net', 'bam.nr-data.net', 'js-agent.newrelic.com', "'unsafe-inline'"],
+        fontSrc: ["'self'", 'amp.azure.net', 'maxcdn.bootstrapcdn.com', 'fonts.gstatic.com'],
+        connectSrc: ["'self'", 'bam.nr-data.net', 'liveproxy-azapp-prod-eastus2-001.azurewebsites.net'],
+        workerSrc: ["'self'", 'amp.azure.net', "blob:"],
+        objectSrc: ["'none'"],
+    }
+}))
+
 app.use(express.static(DIST_DIR, {
     maxAge: '2 days',
     etag: true,
@@ -11,9 +46,7 @@ app.use(express.static(DIST_DIR, {
         res.set('x-timestamp', Date.now())
     }
 }));
-// app.get('*', (req, res) => {
-//     res.sendFile(HTML_FILE)
-// })
+
 const PORT = process.env.PORT || 2112
 app.listen(PORT, () => {
     console.log(`App listening to ${PORT}....`)
